631b77c5829cac7c455c4eac4b12650b827edb36
Ricardo Emerson Julio <ricardoej@gmail.com>
2015-02-20 23:39:56 -0200

    reverting equality optimization

--- src/dynamic_bandwidth_manager/dbm_subscriber.py
+++ src/dynamic_bandwidth_manager/dbm_subscriber.py
@@ -1,27 +1,5 @@
 import rospy
-import pickle
-from std_msgs.msg import String
 
 class DBMSubscriber(rospy.Subscriber):
 	def __init__(self, name, data_class, callback=None, callback_args=None, queue_size=None, buff_size=65536, tcp_nodelay=False):
-		self.primary_data_class = data_class
-		rospy.Subscriber.__init__(self, name, String, callback, callback_args, queue_size, buff_size, tcp_nodelay)
-
-		def decorate_method(method):
-			def wrapper(msg, cb, cb_args):
-				if not hasattr(self, 'last_message_data'):
-					self.last_message_data = None
-
-				if msg.data == '':
-					msg = self.primary_data_class()
-					msg.data = self.last_message_data
-				else:
-					msg.data = pickle.loads(msg.data)
-					self.last_message_data = msg.data
-
-				return method(msg, cb, cb_args)
-
-			return wrapper
-
-		new_function = decorate_method(self.impl._invoke_callback)
-		setattr(self.impl, '_invoke_callback', new_function)
+		rospy.Subscriber.__init__(self, name, data_class, callback, callback_args, queue_size, buff_size, tcp_nodelay)
