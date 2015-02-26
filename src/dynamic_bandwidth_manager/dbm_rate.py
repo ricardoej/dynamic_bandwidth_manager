6b819bc422c8689d429729afc4dcc1e173ae415e
Ricardo Emerson Julio <ricardoej@gmail.com>
2015-02-26 20:24:50 -0300

    receiving publisher object in rate object

--- src/dynamic_bandwidth_manager/dbm_rate.py
+++ src/dynamic_bandwidth_manager/dbm_rate.py
@@ -2,8 +2,8 @@ import rospy
 from .dbm_param import DBMParam
 
 class DBMRate(rospy.Rate):
-	def __init__(self, topic_name, min_frequency, max_frequency, default_frequency):
-		self.topic_name = topic_name
+	def __init__(self, publisher, min_frequency, max_frequency, default_frequency):
+		self.topic_name = publisher.name
 		self.default_frequency = default_frequency
 		DBMParam.create_params(self.topic_name, min_frequency, max_frequency, default_frequency)
 		rospy.Rate.__init__(self, default_frequency)
